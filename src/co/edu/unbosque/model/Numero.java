4d4fe83d13432f026fba0624b858c9ff059d7958
NicolasCamachoL <84592597+NicolasCamachoL@users.noreply.github.com>
2021-05-31 12:26:08 -0500

    creado metodo generarNumeroAleatorio(),

--- src/co/edu/unbosque/model/Numero.java
+++ src/co/edu/unbosque/model/Numero.java
@@ -1,11 +1,30 @@
 package co.edu.unbosque.model;
 
+import java.util.Random;
+
 public class Numero {
     private int numeroAleatorio;
 
-    public Numero(int n) {
-        numeroAleatorio = n;
+    public Numero() {
+        numeroAleatorio = 0;
     }
-    String prueba = "Estoy creando esto para ver como funciona github en eclipse java";
-
+    public Numero(int n) {
+    	numeroAleatorio = n;
+    }
+    
+    public Random generarNumeroAleatorio () { //Metodo para genear el numero aleatorio
+    	Random numeroAleatorio = new Random();
+    	int intAleatorio = numeroAleatorio.nextInt(1000000000); //Rango entre 0 y 999999999
+		return numeroAleatorio;
+		
+    }
+	
+    
+    public int getNumeroAleatorio() {
+		return numeroAleatorio;
+	}
+	public void setNumeroAleatorio(int numeroAleatorio) {
+		this.numeroAleatorio = numeroAleatorio;
+	}
+    
 }
