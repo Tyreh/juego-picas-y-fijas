ab206fb983c88db6f26fd4ddd8b4d4d77deef566
User <User@ThePlayer>
2021-06-12 15:20:48 -0500

    Error pequeño

--- src/co/edu/unbosque/view/VentanaEntrenamiento2.java
+++ src/co/edu/unbosque/view/VentanaEntrenamiento2.java
@@ -21,13 +21,13 @@ public class VentanaEntrenamiento2 extends JDialog {
 	public VentanaEntrenamiento2() {
 		setTitle("Entrenamiento");
 		
-		setSize(500,280);
+		setSize(550,280);
 		setLayout(null);
 		getContentPane().setBackground(Color.cyan);
 		inicializarComponentes();
 		setVisible(false);
 		setLocationRelativeTo(null);
-		setResizable(false);
+		setResizable(true);
 	}
 	public void inicializarComponentes() {
 		Font font = new Font("Century Gothic", Font.ROMAN_BASELINE, 12);
@@ -35,42 +35,42 @@ public class VentanaEntrenamiento2 extends JDialog {
 		
 		eTutorialTitulo = new JLabel("\u2660 ¡Bienvenido a PICAS y FIJAS! \u2660 ");
 		eTutorialTitulo.setFont(font2);
-		eTutorialTitulo.setBounds(5, 0, 500, 20);
+		eTutorialTitulo.setBounds(5, 0, 550, 20);
 		add(eTutorialTitulo);
 		
 		eTutorial1 = new JLabel("- El juego PICAS y FIJAS consiste en adivinar un número entero, el cual es generado ");
 		eTutorial1.setFont(font);
-		eTutorial1.setBounds(5, 30, 500, 20);
+		eTutorial1.setBounds(5, 30, 550, 20);
 		add(eTutorial1);
 		
 		eTutorial2 = new JLabel(" aleatoriamente por el programa.");
 		eTutorial2.setFont(font);
-		eTutorial2.setBounds(5, 50, 500, 20);
+		eTutorial2.setBounds(5, 50, 550, 20);
 		add(eTutorial2);
 		
 		eTutorial3 = new JLabel("- Las PICAS y FIJAS son unas ayudas que suministra el juego para adivinar el número.");
 		eTutorial3.setFont(font);
-		eTutorial3.setBounds(5, 80, 500, 20);
+		eTutorial3.setBounds(5, 80, 550, 20);
 		add(eTutorial3);
 		
 		eTutorial4 = new JLabel("- La cantidad de intentos dependerá de la cantidad de dígitos que contenga el número");
 		eTutorial4.setFont(font);
-		eTutorial4.setBounds(5, 110, 500, 20);
+		eTutorial4.setBounds(5, 110, 550, 20);
 		add(eTutorial4);
 		
 		eTutorial5 = new JLabel("- La FIJA se obtiene si se acierta el dígito y la posicion dentro del número.");
 		eTutorial5.setFont(font);
-		eTutorial5.setBounds(5, 140, 500, 20);
+		eTutorial5.setBounds(5, 140, 550, 20);
 		add(eTutorial5);
 		
 		eTutorial6 = new JLabel("- La PICA se obtiene si se acierta el dígito pero no la posición dentro del número.");
 		eTutorial6.setFont(font);
-		eTutorial6.setBounds(5, 170, 500, 20);
+		eTutorial6.setBounds(5, 170, 550, 20);
 		add(eTutorial6);
 		
 		eTutorial7 = new JLabel("- Hay un mínimo de 3 pistas dependiendo de la cantidad de dígitos dentro del número. ");
 		eTutorial7.setFont(font);
-		eTutorial7.setBounds(5, 199, 500, 20);
+		eTutorial7.setBounds(5, 199, 550, 20);
 		add(eTutorial7);
 
 	}
