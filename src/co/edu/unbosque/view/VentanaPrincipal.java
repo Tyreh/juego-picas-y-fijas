fc1980b75dc3a2b7ee24df81dc5573953f3cc0e7
Tyreh <oscar7u72@gmail.com>
2021-06-14 05:17:41 -0500

    size de ventanaprincipal arreglado

--- src/co/edu/unbosque/view/VentanaPrincipal.java
+++ src/co/edu/unbosque/view/VentanaPrincipal.java
@@ -16,7 +16,7 @@ public class VentanaPrincipal extends JFrame {
     public VentanaPrincipal() {
         fondo = new FondoPrincipal();
         setTitle("Robo al banco");
-        setSize(500, 280);
+        setSize(500, 310);
         setContentPane(fondo);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
