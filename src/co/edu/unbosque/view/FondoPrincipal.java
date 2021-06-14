90634fbb1a551c422933c7bc1a0e22814996bd90
Nelson <User@ThePlayer>
2021-06-14 11:43:43 -0500

    Pequeño error corregido

--- src/co/edu/unbosque/view/FondoPrincipal.java
+++ src/co/edu/unbosque/view/FondoPrincipal.java
@@ -1,22 +1,19 @@
 package co.edu.unbosque.view;
-
 import javax.swing.*;
 import java.awt.*;
-
+/**
+ * Clase que permite añadir una imagen al Frame principal.
+ *@author Oscar Moreno
+ *@author Nicolas Camacho
+ *@author Tomas Espitia
+ *@author Nelson Fandiño
+ */
 public class FondoPrincipal extends JPanel {
-	/**
-	 * Clase que permite añadir una imagen al Frame principal.
-	 *@author Oscar Moreno
-	 *@author Nicolas Camacho
-	 *@author Tomas Espitia
-	 *@author Nelson Fandiño
-	 */
 	
+	/**
+	 * Este método permite presentar una imagen
+	 */
     public void paint(Graphics g) {
-    	/**
-    	 * Este método permite presentar una imagen
-    	 */
-    	
         Image fondo = new ImageIcon("src/Images/Fondo2.jpg").getImage();
         g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
         setOpaque(false);
